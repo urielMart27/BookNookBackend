ce5d0946f8de96e55135cb7752f1e90b1601fe57
Uriel Martinez <martinezuriel651@gmail.com>
2023-12-20 13:45:39 -0600

    Creation of DTO's

---
+++ FullStackAuth_WebAPI/DataTransferObjects/BookDetailsDto.cs
@@ -0,0 +1,8 @@
+﻿namespace FullStackAuth_WebAPI.DataTransferObjects
+{
+    public class BookDetailsDto
+    {
+        public string BookId { get; set; }
+        public string Title { get; set; }
+    }
+}
---
+++ FullStackAuth_WebAPI/DataTransferObjects/ReviewWithUserDto.cs
@@ -0,0 +1,16 @@
+﻿namespace FullStackAuth_WebAPI.DataTransferObjects
+{
+    public class ReviewWithUserDto
+    {
+        public int Id { get; set; }
+        public string Text { get; set; }
+        public double Rating { get; set; }
+        public string BookId { get; set; }
+        public UserForDisplayDto User { get; set; }
+
+
+
+
+
+    }
+}
